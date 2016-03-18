7c9b4a7ca4165824c0b063298078e5a6b55e0d23
Matt Cowski <Matt.Cowski@gmail.com>
2016-03-18 04:12:42 -0500

    sass integration

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 
 gulp.task('express', function() {
   var express = require('express');
@@ -7,7 +8,18 @@ gulp.task('express', function() {
   app.listen(4000, '0.0.0.0');
 });
 
-gulp.task('default', ['express'], function() {
 
+gulp.task('styles', function() {
+    gulp.src('src/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./public/css/'))
 });
+
+gulp.task('default', ['express'], function() {
+	gulp.watch('src/**/*.scss',['styles']);
+});
+
+
+
+
 
